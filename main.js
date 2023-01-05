9cd40e9ea98758fc4f0a36c330fe18d9178b51dc
1245667035@qq.com,EdwinTsaiiiii <1245667035@qq.com>
2023-01-05 16:07:16 +0800

    完成了首页的开发

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
-
-// #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+
+// 导入http相关的库
+import { $http } from '@escook/request-miniprogram'
+uni.$http = $http
+// 请求根路径
+$http.baseUrl = 'https://api-ugo-web.itheima.net'
+
+// 请求拦截器
+$http.beforeRequest = function(option){
+  uni.showLoading({
+    title:'数据加载中'
+  })
+}
+
+// 响应拦截器
+$http.afterRequest = function(option){
+  uni.hideLoading()
+}
+
+// 封装弹框的方法
+uni.$showMsg = function(title='数据请求失败！',duration=1500){
+  uni.showToast({
+    title,
+    duration,
+    icon:'none'
+  })
+}
 
 Vue.config.productionTip = false
 
@@ -11,15 +36,3 @@ const app = new Vue({
     ...App
 })
 app.$mount()
-// #endif
-
-// #ifdef VUE3
-import { createSSRApp } from 'vue'
-import App from './App.vue'
-export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
-}
-// #endif
