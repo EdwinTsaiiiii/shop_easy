ae1e2c4b40f896958e023d75ac5f089b4a583d6a
1245667035@qq.com,EdwinTsaiiiii <1245667035@qq.com>
2023-01-10 14:56:45 +0800

    完成了登录和支付的功能

--- main.js
+++ main.js
@@ -5,29 +5,36 @@ import App from './App'
 import store from './store/store.js'
 
 // 导入http相关的库
-import { $http } from '@escook/request-miniprogram'
+import {
+  $http
+} from '@escook/request-miniprogram'
 uni.$http = $http
 // 请求根路径
 $http.baseUrl = 'https://api-ugo-web.itheima.net'
 
 // 请求拦截器
-$http.beforeRequest = function(option){
+$http.beforeRequest = function(option) {
   uni.showLoading({
-    title:'数据加载中'
+    title: '数据加载中'
   })
+  if (option.url.indexOf('/my/') !== -1) {
+    option.header = {
+      Authorization: store.state.m_user.token
+    }
+  }
 }
 
 // 响应拦截器
-$http.afterRequest = function(option){
+$http.afterRequest = function(option) {
   uni.hideLoading()
 }
 
 // 封装弹框的方法
-uni.$showMsg = function(title='数据请求失败！',duration=1500){
+uni.$showMsg = function(title = '数据请求失败！', duration = 1500) {
   uni.showToast({
     title,
     duration,
-    icon:'none'
+    icon: 'none'
   })
 }
 
@@ -36,7 +43,7 @@ Vue.config.productionTip = false
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App,
-    store
+  ...App,
+  store
 })
 app.$mount()
--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,25 +1,38 @@
 <template>
   <view>
-    My
+    <!-- 用户未登录时，显示登录组件 -->
+    <my-login v-if="!token"></my-login>
+
+    <!-- 用户登录后，显示用户信息组件 -->
+    <my-userinfo v-else></my-userinfo>
   </view>
 </template>
 
 <script>
   // 导入自己封装的 mixin 模块
   import badgeMix from '@/mixins/tabbar-badge.js'
+  import {
+    mapState
+  } from 'vuex'
   export default {
-    mixins:[badgeMix],
+    mixins: [badgeMix],
     data() {
       return {
-        
+
       }
     },
+    computed: {
+      ...mapState('m_user', ['token'])
+    },
     methods: {
-      
+
     }
   }
 </script>
 
 <style>
-
+  page,
+  .my-container {
+    height: 100%;
+  }
 </style>
